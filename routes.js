7bf7d0402a2337c3035618ba52e119120135ea6a
Alexsandro Frizon <83145774+AlexsandroFrizon@users.noreply.github.com>
2023-05-12 09:50:24 -0300

    Update routes.js
    corrigindo

--- routes.js
+++ routes.js
@@ -1,8 +1,10 @@
 // Importando as dependências do projeto
 const express = require('express');
+
 // Referencia ao db.js
 const db = require("./db");
 const routes = express.Router();
+
 // associa as rotas ao seu método do db.js
 routes.get('/authors', db.selectAuthors);
 routes.post('/authors', db.insertAuthor);
